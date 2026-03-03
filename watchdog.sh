d0946d55024ebfbed6777b39ba00077781978f15
soapsama1337 <wzk1345290301@gmail.com>
2026-03-04 03:22:59 +0800

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
 # 检查 v2bx 状态
-status=$(systemctl is-active v2bx)
-
-# 如果状态不是 active
-if [ "$status" != "active" ]; then
-    # 记录时间戳日志
+if [ "$(systemctl is-active v2bx)" != "active" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - 状态异常，执行 v2bx restart" >> /var/log/v2bx_monitor.log
-    
-    # 删掉了会导致报错的内存清理命令，直接执行重启
     v2bx restart >> /var/log/v2bx_monitor.log 2>&1
 fi
